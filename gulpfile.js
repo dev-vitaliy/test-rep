b0a87258b2e048b2f87e1e80341adc90a81be7cb
Vitaliy <vitalik88@yandex.ru>
2017-04-24 10:28:11 +0300

    my first commit

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+ var gulp = require('gulp');
+ var sass = require('gulp-sass');
+ 
+ 
+ gulp.task('default', function(){
+	 
+	 gulp.watch('scss/*.scss', function(e){
+		 gulp.src(e.path)
+	         .pipe(sass())
+		     .pipe(gulp.dest('css'))
+	 })
+	 
+	 
+ });
